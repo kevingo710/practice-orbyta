9f3857b6e01845c9de1ce776632ea0a82a53695e
Kevin Gracia <kevingracia07@gmail.com>
2024-09-29 15:58:51 +0200

    feat: create CustomInput

---
+++ src/presentation/components/CustomInput/CustomInput.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { TextInput, StyleSheet, View, Text, TextInputProps } from 'react-native';
+
+interface CustomInputProps {
+  value: string;
+  onChangeText: (text: string) => void;
+  placeholder?: string;
+  label?: string;
+  keyboardType?: TextInputProps['keyboardType'];
+}
+
+const CustomInput: React.FC<CustomInputProps> = ({
+  value,
+  onChangeText,
+  placeholder = 'Enter text',
+  keyboardType = 'default',
+  label = 'default',
+}) => {
+  return (
+    <View>
+      {label ? <Text style={styles.labelText}>{label}</Text> : <></>}
+      <TextInput
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+        value={value}
+        keyboardType={keyboardType}
+        style={styles.input}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+    color: 'black',
+    borderWidth: 1,
+    height: 50,
+    marginTop: 10,
+    marginBottom: 20,
+    padding: 10,
+  },
+  labelText: {
+    color: 'black'
+  }
+});
+
+export default CustomInput;
